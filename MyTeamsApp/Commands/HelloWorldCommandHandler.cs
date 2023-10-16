f62a9dc1763fe5099bea6b6154ae71c64185014a
Ketan Chawda <kechaw@microsoft.com>
2023-10-16 22:52:30 +0530

    Add project files.

---
+++ MyTeamsApp/Commands/HelloWorldCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using MyTeamsApp.Models;
+using AdaptiveCards.Templating;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.TeamsFx.Conversation;
+using Newtonsoft.Json;
+using AdaptiveCards;
+
+namespace MyTeamsApp.Commands
+{
+    /// <summary>
+    /// The <see cref="HelloWorldCommandHandler"/> registers a pattern with the <see cref="ITeamsCommandHandler"/> and
+    /// responds with an Adaptive Card if the user types the <see cref="TriggerPatterns"/>.
+    /// </summary>
+    public class HelloWorldCommandHandler : ITeamsCommandHandler
+    {
+        private readonly ILogger<HelloWorldCommandHandler> _logger;
+        private readonly string _adaptiveCardFilePath = Path.Combine(".", "Resources", "HelloWorldCard.json");
+
+        public IEnumerable<ITriggerPattern> TriggerPatterns => new List<ITriggerPattern>
+        {
+            // Used to trigger the command handler if the command text contains 'helloWorld'
+            new RegExpTrigger("helloWorld")
+        };
+
+        public HelloWorldCommandHandler(ILogger<HelloWorldCommandHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<ICommandResponse> HandleCommandAsync(ITurnContext turnContext, CommandMessage message, CancellationToken cancellationToken = default)
+        {
+            _logger?.LogInformation($"App received message: {message.Text}");
+
+            // Read the Adaptive Card JSON from the file
+            string cardJson = File.ReadAllText(Path.Combine(".", "Resources", "HelloWorldCard.json"));
+
+            // Parse the JSON into an AdaptiveCard object
+            var adaptiveCard = AdaptiveCard.FromJson(cardJson).Card;
+
+            var attachment = new Attachment
+            {
+                ContentType = AdaptiveCard.ContentType,
+                Content = adaptiveCard
+            };
+            var reply = MessageFactory.Attachment(attachment);
+            // send response
+            return new ActivityCommandResponse(reply);
+        }
+    }
+}
---
+++ MyTeamsApp/TeamsBot.cs
@@ -0,0 +1,86 @@
+﻿using AdaptiveCards;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Teams;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace MyTeamsApp
+{
+    /// <summary>
+    /// An empty bot handler.
+    /// You can add your customization code here to extend your bot logic if needed.
+    /// </summary>
+    public class TeamsBot : TeamsActivityHandler
+    {
+        public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
+        {
+            if (turnContext.Activity.Type == "message")
+            {
+                var reply = MessageFactory.Attachment(CreateAdaptiveCardAttachment());
+                await turnContext.SendActivityAsync(reply, cancellationToken);
+            }
+            else if (turnContext.Activity.Type == "invoke" && turnContext.Activity.Name == "adaptiveCard/action")
+            {
+                await OnAdaptiveCardInvokeAsync(turnContext,cancellationToken);
+            }
+        }
+
+        private Attachment CreateAdaptiveCardAttachment()
+        {
+            // Read the Adaptive Card JSON from the file
+            string cardJson = File.ReadAllText(Path.Combine(".", "Resources", "HelloWorldCard.json"));
+
+            // Parse the JSON into an AdaptiveCard object
+            var adaptiveCard = AdaptiveCard.FromJson(cardJson).Card;
+
+            var attachment = new Attachment
+            {
+                ContentType = AdaptiveCard.ContentType,
+                Content = adaptiveCard
+            };
+
+            return attachment;
+        }        
+
+        public async Task OnAdaptiveCardInvokeAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            if (turnContext.Activity.Value is JObject invokeValue)
+            {
+                if (invokeValue["action"]["verb"].ToString() == "qwerty")
+                {
+                    // Process the submission data and decide whether to hide 'text box'
+                    bool shouldHideField = true;
+
+                    var card = File.ReadAllText(Path.Combine(".", "Resources", "HelloWorldCard.json"));
+                    var adaptiveCard = AdaptiveCard.FromJson(card).Card;
+
+                    // Update the Adaptive Card JSON to hide 'hiddenField'
+                    if (shouldHideField)
+                    {
+                        var inputField = adaptiveCard.Body.OfType<AdaptiveTextInput>().FirstOrDefault(input => input.Id == "defaultInputId");
+                        if (inputField != null)
+                        {
+                            adaptiveCard.Body.OfType<AdaptiveTextInput>().FirstOrDefault(input => input.Id == "defaultInputId").IsVisible = false;
+                        }
+                    }
+
+                    // Send the updated Adaptive Card back to the user
+                    var updatedCardAttachment = new Attachment
+                    {
+                        ContentType = AdaptiveCard.ContentType,
+                        Content = adaptiveCard,
+                    };
+                    var activity = MessageFactory.Attachment(updatedCardAttachment);
+                    activity.Id = turnContext.Activity.ReplyToId;
+                    await turnContext.UpdateActivityAsync(activity, cancellationToken);
+                   
+                }
+            }
+        }
+
+
+
+    }
+}
